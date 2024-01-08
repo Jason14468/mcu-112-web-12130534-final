385a285917c2825c7e6eebb7cd68cb0aaad54bf2
Jason14468 <chenyenting08@gmail.com>
2024-01-08 16:53:15 +0800

    (feat):建立綠色背景的部分與conduit詞語，還有網頁裡的一些元件的距離

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
