ae084c32c11e87c5c9ffdd441cadd56462952681
Jason14468 <chenyenting08@gmail.com>
2024-01-08 16:39:15 +0800

    把導覽列搬遷至nav-bar的地方

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
