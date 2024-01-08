feaab8d7f9451c168d6a8fcf2037824ebf43b0f6
Jason14468 <chenyenting08@gmail.com>
2024-01-09 00:33:32 +0800

    (feat):修改間距與建立頁腳

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavBarComponent, HeaderComponent],
+  imports: [NavBarComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
